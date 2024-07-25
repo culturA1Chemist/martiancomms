742beea45dc53975b2f310e5dc1d669cf1ce0aa1
Mars de Gonzalez <mars@martiancommunicationsllc.com>
2024-07-25 12:43:24 -0400

    Initial Commit.

---
+++ polls/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline): # StackedInline creates a more dispersed view with header bars repeating the choice text (though this is probably editable)
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": [
+            "question_text"
+        ]}),
+        ("Date Information", {
+            "fields": ["pub_date"],
+            "classes": ["collapse"],
+        }),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
