dadd00d09c1723136a23b0ee76b1cb84eaa5dd3c
Mars de Gonzalez <mars@martiancommunicationsllc.com>
2024-07-29 23:46:11 -0400

    created members app and basic views and initial pages app

--- martiancomms/settings.py
+++ martiancomms/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
     'blog.apps.BlogConfig',
+    'members.apps.MembersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
