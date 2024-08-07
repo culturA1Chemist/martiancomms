0f9245725f054fa6665c3a9631dbaf68baa55313
Mars de Gonzalez <mars@martiancommunicationsllc.com>
2024-08-06 21:56:38 -0400

    static pages app inheritance structure, simple style changes, and tests

--- martiancomms/settings.py
+++ martiancomms/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pages.apps.PagesConfig'
+    'pages.apps.PagesConfig',
     'blog.apps.BlogConfig',
     'members.apps.MembersConfig',
     'polls.apps.PollsConfig',
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
